9d8999c6ef30013ca1bae27ba5b9621bdbd23d05
Mikko Lipsanen <lipsanenm@gmail.com>
2023-08-16 12:24:00 +0300

    Changed 'url' variable to 'path'

--- api.py
+++ api.py
@@ -105,12 +105,11 @@ async def postit(file: UploadFile = File(...)):
 async def postit_url(path: str):
     try:
         # Loads the image from the path sent with the GET request
-        #req_content = requests.get(url)
         image = Image.open(path)
         image.draft('RGB', (IMG_SIZE, IMG_SIZE))
 
     except Exception as e:
-        logging.error('Failed to recognize file %s as an image. Error: %s' % (url, e))
+        logging.error('Failed to recognize file %s as an image. Error: %s' % (path, e))
         raise HTTPException(status_code=400, detail='Failed to load the input image file: %s' % e)
 
     # Get predicted class and confidence
